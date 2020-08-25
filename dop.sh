12ff2d344cdc11d9582ca956fcd42383b8d86b27
Amit Bhatt <33670159+amitbhatt818@users.noreply.github.com>
2020-08-25 18:56:22 +0530

    Changed code according to linting

--- dop.sh
+++ dop.sh
@@ -9,14 +9,14 @@ chmod 700 get_helm.sh
 helm version
 
 ## Creates secret
-kubectl create secret docker-registry directoronprem-registry-secret --docker-server=registry.mayadata.io --docker-username=${{ DOCKER_USERNAME }} --docker-password=${{ DOCKER_PASSWORD }}
+kubectl create secret docker-registry directoronprem-registry-secret --docker-server=registry.mayadata.io --docker-username=$'{{ DOCKER_USERNAME }}' --docker-password=$'{{ DOCKER_PASSWORD }}'
 kubectl get secret
 
 ## Install Dop 
-IP=$(sudo kubectl get nodes -o wide --no-headers | awk {'print $6'} | head -n 1)
+IP=$(sudo kubectl get nodes -o wide --no-headers | awk '{print $6}' | head -n 1)
 URL=http://$IP
 helm repo add kubera https://charts.mayadata.io/
 helm repo update
-helm install kubera kubera/kubera-charts --set server.url=$URL
+helm install kubera kubera/kubera-charts --set server.url="$URL"
 sleep 50
 kubectl get pod
